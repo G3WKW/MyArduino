f9eba440db0031fe41709a8f6e19cc58bce6fe85
Bob Thornton <g3wkw.bob@gmail.com>
2020-02-28 10:20:11 +0000

    Update MyAD9850.ino

--- MyAD9850/MyAD9850.ino
+++ MyAD9850/MyAD9850.ino
@@ -8,27 +8,35 @@ double freq=10000000;
 double rxfreq = 144.950;
 double txfreq = 144.950;
 double trimFreq = 124999570;
+//define array of input pins for 19,20,21
+//define array of TX frequs
+//define array of Rx freqs
+
 
 int phase = 0;
 
 void setup(){
+  //set array of pins to inputs
   pinMode(TX, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   DDS.begin(W_CLK_PIN, FQ_UD_PIN, DATA_PIN, RESET_PIN);
   DDS.calibrate(trimFreq);
    DDS.up();
+   
 }
 
 void loop(){
+  //read and storethe input pin value
+  //set freq from array
   freq=(rxfreq - 10.7)*1000000/12;
   DDS.setfreq(freq, phase);
   digitalWrite(LED_BUILTIN, LOW); 
-  while(!digitalRead(TX));
+  while(!digitalRead(TX));  //  and read pin = stored value
  
   freq=txfreq*1000000/16;
   DDS.setfreq(freq, phase);
   digitalWrite(LED_BUILTIN, HIGH); 
-  while(digitalRead(TX));
+  while(digitalRead(TX));  //  and read pin = stored value
   
   
  
